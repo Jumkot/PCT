01cdcbdaf9c38d3774c4c3572912e4a9e159ad3c
Анастасия Бондаренко <is241s03@oak.ipa.csc.sibsutis.ru>
2024-12-05 00:01:51 +0700

    First oak's results

--- 5_sem/course_work/inverse.cpp
+++ 5_sem/course_work/inverse.cpp
@@ -164,6 +164,8 @@ bool inverse_matrix(double* start, double* inverse, int current_col) {
 }
 
 int main(int argc, char **argv) {
+    std::cout.setf(std::ios::fixed);
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
